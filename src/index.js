a952c9a9ba27d37808339087b7a7f3f1242e1551
Jonathan Adly <jonathanadly@Jonathans-MacBook-Air.local>
2021-09-21 14:48:20 -0400

    festival bulk send

--- src/index.js
+++ src/index.js
@@ -1,17 +1,114 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from 'react'
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+
+function Square(props) {
+
+
+    return (
+        <button className="square" onClick={() => props.onClick()}>
+            {props.value}
+        </button>
+    );
+
+}
+
+function Board() {
+
+    const [squares, setSquares] = useState([null, null, null, null, null, null, null, null, null])
+    const [xIsNext, setTurn] = useState(true)
+
+    function renderSquare(i) {
+        return <Square
+            value={squares[i]}
+            onClick={() => handleClick(i)}
+        />;
+    }
+
+    function handleClick(i) {
+
+        const newSquares = squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        if (xIsNext) {
+            newSquares[i] = "X";
+            setTurn(false)
+        }
+        else {
+            newSquares[i] = "O"
+            setTurn(true)
+        }
+        setSquares(newSquares)
+    }
+
+    const winner = calculateWinner(squares)
+
+
+    return (
+        <div>
+            <div className="status"> {winner ? "Winner:" + winner : xIsNext ? 'Next player: X' : "Next player: O"}</div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
+
+
+function Game() {
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+            </div>
+        </div>
+    );
+}
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
